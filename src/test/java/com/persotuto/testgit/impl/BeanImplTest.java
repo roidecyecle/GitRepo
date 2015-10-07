ed5cd7857b716637e753be582f75ba6bad553bef
saddem <Saddem@Saddem-PC>
2015-10-07 22:39:01 +0100

    changes

---
+++ src/test/java/com/persotuto/testgit/impl/BeanImplTest.java
@@ -0,0 +1,13 @@
+package com.persotuto.testgit.impl;
+
+import junit.framework.TestCase;
+import com.persotuto.testgit.Bean;
+
+public class BeanImplTest extends TestCase {
+
+    public void testBeanIsABean() {
+	Bean aBean = new BeanImpl();
+        assertTrue(aBean.isABean());
+    }
+
+}
